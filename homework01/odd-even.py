a0576f6c55f8218764ff4a2b981e1a9481587866
cskevofilax@utexas.edu <costaki@isp02.tacc.utexas.edu>
2022-01-31 23:30:08 -0600

    tweeked odd-even.py so that it is checking through the list at interval at i

--- homework01/odd-even.py
+++ homework01/odd-even.py
@@ -1,12 +1,15 @@
-#Creating a function to determin if a list of integers is either even or odd
+# Creating a function to determin if a list of integers is either even or odd
 
+# A list of integers 
 nums = [33, 2, 7, 9, 4, 100, 254, 1329, 96, 24]
 
+# Function to see if the nums of index [i] is either even or odd
 def even_odd(nums):
 	for i in range(len(nums)):
-		if nums % 2 == 1:
-			print(nums[i], " :odd")
+		# Checks to see if the remainder is 1, if it 1, tbe value is odd, else 			it is even 
+		if nums[i] % 2 == 1:
+			print("Odd: ", nums[i])
 		else:
-			print(nums[i], " :even")
+			print("Even: ", nums[i])
 even_odd(nums) 
 
