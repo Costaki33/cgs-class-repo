116b637f7dd3fcde2b1ee2b66a15fc2a38e3dbc0
cskevofilax@utexas.edu <costaki@isp02.tacc.utexas.edu>
2022-01-31 23:03:14 -0600

    directory named homework01 created to store hw files

---
+++ homework01/odd-even.py
@@ -0,0 +1,13 @@
+#Creating a function to determin if a list of integers is either even or odd
+
+nums = [33, 2, 7, 9, 4, 100, 254, 1329, 96, 24]
+
+def even_odd(nums):
+	for i in range(len(even_odd)):
+		if nums % 2 == 1:
+			print(nums[i], " :odd")
+		else:
+			print(nums[i], " :even")
+
+:
+
