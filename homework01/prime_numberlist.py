424c12b3048136c65b24d3afb1c78724e0e6914e
cskevofilax@utexas.edu <costaki@isp02.tacc.utexas.edu>
2022-02-01 01:10:37 -0600

    added new file called prime_numberlist.py that finds all the primes from (3,100

--- homework01/prime_numberlist.py
+++ homework01/prime_numberlist.py
@@ -1,3 +1,15 @@
 # This program iterates over over every integer from 3 to 100 and prints out the number only if it is a prime number
 
-def prime:
+def prime(x, y):
+
+	for num in range(x, y):
+		check = 0 
+		for i in range(2, (num//2 + 1)):
+			if(i % num == 0):
+				check = check + 1
+				break
+		if (check == 0 and num != 1):
+			print(" %d" %num, end = " ")
+
+prime(3, 100)
+
